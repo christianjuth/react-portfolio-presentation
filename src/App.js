a9755a194ed4e129d7999b7c4df02ef2a532bf9c
Christian Juth <cjuth2@gmail.com>
2020-12-06 20:42:17 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import Div100vh from 'react-div-100vh'
+import styles from './App.module.css';
+
+function Link({
+  external = false,
+  href,
+  children,
+  className
+}) {
+  return (
+    <a 
+    className={className}
+      href={href}
+      rel={external ? 'noopener nofollow' : ''}
+    >
+      {children}
+    </a>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div100vh>
+      <div className={styles.app}>
+        <div className={styles.page}>
+          <header className={styles.layout}>
+            <img 
+              className={styles.profilePicture}
+              src='https://i.imgur.com/iHXeY1G.jpg'
+            />
+            <div>
+              <span>Jim Carrey</span>
+              <h1>Canadian-American actor, comedian, writer, producer, and artist.</h1>
+            </div>
+          </header>
+
+          <div 
+            className={styles.spacer}
+          />
+
+          <main className={styles.layout}>
+            <aside>
+              <h2>Projects</h2>
+              <ul>
+                <li>
+                  {'🐬 '}
+                  <Link href="#" external>
+                    Ace Ventura
+                  </Link>
+                </li>
+
+                <li>
+                  {'🙏 '}
+                  <Link href="#" external>
+                    Bruce Almighty
+                  </Link>
+                </li>
+
+                <li>
+                  {'🤪 '}
+                  <Link href="#" external>
+                    Dumb and Dumber
+                  </Link>
+                </li>
+
+                <li>
+                  {'📺 '}
+                  <Link href="#" external>
+                    The Trueman Show
+                  </Link>
+                </li>
+
+                
+              </ul>
+            </aside>
+            <article>
+              <h2>Background</h2>
+              <p>Comedian and actor Jim Carrey was born in Newmarket, Ontario, Canada, on January 17, 1962. Carrey relocated to Los Angeles to pursue comedy, eventually landing a spot on the sketch comedy show In Living Color. He went on to huge box office success in comedies, including Ace Ventura: Pet Detective and The Mask, and delivered acclaimed dramatic performances in The Truman Show, Man on the Moon, and Eternal Sunshine of the Spotless Mind. His later films include Kick Ass 2 and Dumb and Dumber To.</p>
+              <Link
+                href="mailto:jim"
+              />
+            </article>
+          </main>
+        </div>
+      </div>
+    </Div100vh>
   );
 }
 
